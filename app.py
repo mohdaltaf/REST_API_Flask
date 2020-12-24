a56ca7ef49efb58199faa5d064cfdb4b547c2fb4
Mohdaltaf <mohdaltafhajam@gmail.com>
2020-12-25 01:21:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.secret_key = 'altaf'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 jwt = JWT(app, authenticate, identity)
 
